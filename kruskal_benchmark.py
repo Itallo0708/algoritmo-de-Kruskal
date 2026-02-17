a246ce704c0901de7479a871e2dc54e6d9bb5fd3
Itallo0708 <itallofcn@gmail.com>
2026-02-17 00:54:04 -0300

    feat: adiciona rotina de benchmark variando número de arestas
    - Implementa função executar_testes
    - Define número fixo de vértices (1000)
    - Testa diferentes quantidades de arestas
    - Gera grafos aleatórios para cada cenário
    - Executa algoritmo de Kruskal
    - Armazena e imprime tempo de execução

--- kruskal_benchmark.py
+++ kruskal_benchmark.py
@@ -1,5 +1,6 @@
 # import das bibliotecas
 import random
+from kruskal import KruskalAGM
 
 # Função para gerar grafos
 def gerar_grafos(numero_arestas, numero_vertices):
@@ -15,3 +16,23 @@ def gerar_grafos(numero_arestas, numero_vertices):
         arestas.append([u, v, peso])
 
     return arestas
+
+# Função para executar os testes com diferentes tamanhos de grafos
+def executar_testes():
+    # Teste com variação de arestas
+    print("Iniciando Teste a Arestas\n")
+    
+    v_fixo = 1000
+    lista_numero_arestas = [2000, 5000, 10000, 5000]
+    resultados = []
+
+    for tamanho in lista_numero_arestas:
+        g = KruskalAGM(v_fixo)
+        arestas = gerar_grafos(tamanho, v_fixo)
+
+        for u, v, w in arestas:
+            g.add_aresta(u, v, w)
+        
+        tempo, _= g.kruskal()
+        resultados.append(tempo)
+        print(f"V: {v_fixo}; Arestas: {tamanho}; tempo: {tempo}")
