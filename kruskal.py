9337fca7bce61f1efb7a99cbf609f261d5318161
Itallo0708 <itallofcn@gmail.com>
2026-02-17 00:22:21 -0300

    feat: implementa método kruskal com cálculo de AGM e benchmark
    - Ordena as arestas pelo peso
    - Inicializa estruturas Union-Find (parent e rank)
    - Implementa laço principal do algoritmo de Kruskal
    - Evita ciclos utilizando find e union
    - Calcula peso total da Árvore Geradora Mínima
    - Adiciona medição de tempo com time.perf_counter
    - Retorna tempo de execução e peso da AGM

--- kruskal.py
+++ kruskal.py
@@ -31,3 +31,49 @@ class KruskalAGM:
         else:
             parent[raiz_y] = raiz_x
             rank[raiz_x] += 1
+    
+    # Execução do algoritmo
+    def kruskal(self):
+        resultado = []
+
+        # Arestas ordenadas
+        i = 0
+        # Arestas na AGM
+        e = 0
+
+        # Início da medição de tempo
+        start_time = time.perf_counter()
+
+        # 1. Ordenar arestas pelo peso
+        self.grafo = sorted(self.grafo, key=lambda item: item[2])
+
+        parent = []
+        rank = []
+
+        # Inicializa V subconjuntos unitários para os vertices
+        for node in range (self.V):
+            parent.append(node)
+            rank.append(0)
+        
+        # 2. Iterar pelas arestas ordenadas
+        while e < self.V -1 and i < len(self.grafo):
+            u, v, w = self.grafo[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+
+            # Se não forma ciclo, adiciona na AGM
+            if x != y:
+                e = e + 1
+                resultado.append([u, v, w])
+                self.union(parent, rank, x, y)
+        
+        # Fim da medição de tempo
+        end_time = time.perf_counter()
+
+        tempo_execucao = end_time - start_time
+
+        pesoAGM = sum([aresta[2] for aresta in resultado])
+
+        return tempo_execucao, pesoAGM
+
