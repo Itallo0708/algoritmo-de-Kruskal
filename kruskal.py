060351f38ff60984e54a8f8d7117635d78bc1710
Itallo0708 <itallofcn@gmail.com>
2026-02-16 23:28:23 -0300

    feat: implementa estrutura Union-Find com Path Compression e Union by Rank
    - Adiciona método find para localizar o representante do conjunto
    - Implementa compressão de caminho (Path Compression) para otimizar buscas
    - Adiciona método union para unir conjuntos distintos
    - Aplica estratégia Union by Rank para manter a árvore balanceada

--- kruskal.py
+++ kruskal.py
@@ -12,4 +12,22 @@ class KruskalAGM:
     def add_aresta(self, aresta,  vertice, peso):
         self.grafo.append([aresta, vertice, peso])
 
+    # Encontra o representante do conjunto do qual o vertice i pertence.
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        parent[i] = self.find(parent, parent[i])
+        return parent[i]
     
+    # Une os conjuntos 
+    def union(self, parent, rank, x, y):
+        raiz_x = self.find(parent, x)
+        raiz_y = self.find(parent, y)
+
+        if rank[raiz_x] < rank[raiz_y]:
+            parent[raiz_x] = raiz_y
+        elif rank[raiz_x] > rank[raiz_y]:
+            parent[raiz_y] = raiz_x
+        else:
+            parent[raiz_y] = raiz_x
+            rank[raiz_x] += 1
