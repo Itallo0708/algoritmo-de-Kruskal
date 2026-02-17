09beb064d8e23ade1e409debc39a445c41622168
Itallo0708 <itallofcn@gmail.com>
2026-02-16 23:07:57 -0300

    feat: implementa estrutura inicial da classe KruskalAGM
    - Adiciona import da biblioteca time para benchmark
    - Cria classe KruskalAGM
    - Define construtor com número de vértices (self.V)
    - Inicializa lista de arestas (self.grafo)
    - Implementa método add_aresta para inserir arestas com peso

--- kruskal.py
+++ kruskal.py
@@ -1,1 +1,15 @@
-print('teste')
+#Importando biblioteca para a realização do Benchmark
+import time
+
+# Classe da arvore geradora minima com Kruskel
+class KruskalAGM:
+    def __init__(self, vertices):
+        self.V = vertices
+        # lista das arestas
+        self.grafo = [] 
+    
+    # Adiciona arestas ao grfo
+    def add_aresta(self, aresta,  vertice, peso):
+        self.grafo.append([aresta, vertice, peso])
+
+    
