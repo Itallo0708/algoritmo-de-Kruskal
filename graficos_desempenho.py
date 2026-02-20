dd4e3b8c53e7f118180db81c153f1f1167e4b168
Itallo0708 <itallofcn@gmail.com>
2026-02-20 15:37:19 -0300

    fix(graficos_desempenho): Ajusta o título.

--- graficos_desempenho.py
+++ graficos_desempenho.py
@@ -11,7 +11,10 @@ def gerar_grafico(tamanhos, tempos_kruskal, tempos_prim = None, nome_variavel = 
     if tempos_prim:
         plt.plot(tamanhos, tempos_prim, marker='s', color='red', linestyle='--', linewidth=2, label='Prim')
     
-    plt.title(f"Análise de Sensibilidade a {nome_variavel}")
+    if nome_variavel=="Arestas":
+        plt.title(f"Análise de Sensibilidade a {nome_variavel} com 100000 vertices")
+    if nome_variavel=="Vertices":
+        plt.title(f"Análise de Sensibilida a {nome_variavel} com 50000 arestas")
     plt.xlabel(f"Número de {nome_variavel}")
     plt.ylabel(f"tempo de Execução (segundos)")
     plt.grid(True, linestyle=':', alpha=0.7)
