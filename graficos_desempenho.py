20e672f780548d2008a25bebbeb3f4b2da281272
Itallo0708 <itallofcn@gmail.com>
2026-02-19 21:55:01 -0300

    feat: Adiciona uma função para plotar os gráficos comparativos.

---
+++ graficos_desempenho.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+# Função para gerar um gráfico comparativo
+
+def gerar_grafico(tamanhos, tempos_kruskal, tempos_prim = None, nome_variavel = ""):
+    plt.figure(figsize=(10, 6))
+
+    # Linha de desempenho do Kruskal
+    plt.plot(tamanhos, tempos_kruskal, marker='o', color='blue', linewidth=2, label='Kruskal')
+
+    # Linha de desempenho do prim
+    if tempos_prim:
+        plt.plot(tamanhos, tempos_prim, marker='s', color='red', linestyle='--', linewidth=2, label='Prim')
+    
+    plt.title(f"Análise de Sensibilidade a {nome_variavel}")
+    plt.xlabel(f"Número de {nome_variavel}")
+    plt.ylabel(f"tempo de Execução (segundos)")
+    plt.grid(True, linestyle=':', alpha=0.7)
+    plt.legend()
+    
+    plt.show()
