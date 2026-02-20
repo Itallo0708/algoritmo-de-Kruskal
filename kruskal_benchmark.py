5a8b6678c0502a711ad485f8d80b5727e2c5bf13
Itallo0708 <itallofcn@gmail.com>
2026-02-19 21:39:02 -0300

    feat: corrige nome da variável de resultados e organiza testes de sensibilidade a vértices

--- kruskal_benchmark.py
+++ kruskal_benchmark.py
@@ -9,7 +9,7 @@ def executar_testes():
     
     v_fixo = 1000
     lista_numero_arestas = [2000, 5000, 10000, 5000]
-    resultados = []
+    resultados_arestas = []
 
     for tamanho in lista_numero_arestas:
         g = KruskalAGM(v_fixo)
@@ -19,5 +19,26 @@ def executar_testes():
             g.add_aresta(u, v, w)
         
         tempo, _= g.kruskal()
-        resultados.append(tempo)
+        resultados_arestas.append(tempo)
         print(f"V: {v_fixo}; Arestas: {tamanho}; tempo: {tempo}")
+    
+    # Teste com variação no número de vértice
+    # Número fixo de arestas = 10000
+
+    print("\n Iniciando Teste de Sensibilidade a Vértices...")
+    arestas_fixas = 10000
+    lista_numero_vertices = [500, 1000, 5000, 10000]
+    resultados_vertice = []
+
+    for v in lista_numero_vertices:
+        g = KruskalAGM(v)
+        arestas = gerar_grafos(v, arestas_fixas)
+
+        for u, v_adj, w in arestas:
+            g.add_aresta(u, v_adj, w)
+
+        tempo, _ = g.kruskal()
+        resultados_vertice.append(tempo)
+        print(f"Número de Vértices: {v}\n Arestas: {arestas_fixas}\n Tempo: {tempo:.6f}s")
+
+    return lista_numero_arestas, resultados_arestas, lista_numero_vertices, resultados_vertice
